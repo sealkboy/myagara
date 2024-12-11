432a5a58f1a40faf59b76d1e88361e72f0a277af
unknown <sealkevinboyabad@gmail.com>
2024-12-11 03:19:13 -0800

    Initial Commit: Spring Boot Maven Project

---
+++ src/main/java/dev/sealkboy/myagara/MyagaraApplication.java
@@ -0,0 +1,13 @@
+package dev.sealkboy.myagara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyagaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyagaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sealkboy/myagara/MyagaraApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sealkboy.myagara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyagaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
