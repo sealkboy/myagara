c8bfa85efd7f5ff79ed112175bd57f70aea03fe6
unknown <sealkevinboyabad@gmail.com>
2024-12-11 14:27:54 -0800

    Returned to previous version because of problems with folder structure when added python and ml model files

--- src/main/java/dev/sealkboy/myagara/MyagaraApplication.java
+++ src/main/java/dev/sealkboy/myagara/MyagaraApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyagaraApp {
+public class MyagaraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyagaraApplication.class, args);
